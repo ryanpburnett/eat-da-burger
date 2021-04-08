9c0966b75cae7f7aa85ce58ca60a0e3c620d90e3
Ryan <ryanpburnett@yahoo.com>
2021-04-08 13:05:29 -0500

    seeds and schema established

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burgerator', false);
+INSERT INTO burgers (burger_name) VALUES ('BurgerTron 5000', true);
+INSERT INTO burgers (burger_name) VALUES ('Night of the Living Burger', false);
