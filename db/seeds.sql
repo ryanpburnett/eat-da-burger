c5f74f9cc318f1c22108a632d69e1c02ad53decb
Ryan <ryanpburnett@yahoo.com>
2021-04-11 15:29:35 -0500

    fixed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Burgerator', false);
-INSERT INTO burgers (burger_name) VALUES ('BurgerTron 5000', true);
-INSERT INTO burgers (burger_name) VALUES ('Night of the Living Burger', false);
-INSERT INTO burgers (burger_name) VALUES ('Para Bailar La Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BurgerTron 5000', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Night of the Living Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Para Bailar La Burger', true);
