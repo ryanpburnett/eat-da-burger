f86bf17520e4cc457d8597e4caaa46d9987f46d1
Ryan <ryanpburnett@yahoo.com>
2021-04-11 09:01:03 -0500

    requiring in more packages on server, work on middleware

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,10 +2,16 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-// CHANGE LATER, EXISTING CODE NEEDS WORK
-
-var express = require("express");
-
-var PORT = process.env.PORT || 3000;
-
-var app = express();
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgerController.js");
+const routes = require("./controllers/burgerController.js");
 
 app.use(routes);
 
